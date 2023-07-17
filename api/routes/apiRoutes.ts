3ee1ef15e57a1b13a7483049ebf45213a2d9543f
thawzin.dev <thawzin99777@gmail.com>
2023-07-17 21:34:58 +0630

    update

--- api/routes/apiRoutes.ts
+++ api/routes/apiRoutes.ts
@@ -1,25 +1,25 @@
-import express from 'express'
+import express from 'express';
 
-import loginController from '../controllers/loginController'
-import schemeValidator from '../../middleware/schemeValidator'
-import createUserScheme from '../schemes/users/create.scheme'
-import loginUserScheme from '../schemes/users/login.scheme'
-import userController from '../controllers/userController'
-import { verifyToken } from '../../middleware/auth'
-const router = express.Router()
+import loginController from '../controllers/loginController';
+import schemeValidator from '../../middleware/schemeValidator';
+import createUserScheme from '../schemes/users/create.scheme';
+import loginUserScheme from '../schemes/users/login.scheme';
+import userController from '../controllers/userController';
+import { verifyToken } from '../../middleware/auth';
+const router = express.Router();
 
 router.post(
     '/user/register',
     schemeValidator(createUserScheme),
     loginController.register
-)
+);
 
-router.get(
+router.post(
     '/user/login',
     schemeValidator(loginUserScheme),
     loginController.login
-)
+);
 
-router.get('/user/profile', verifyToken, userController.profile)
+router.get('/user/profile', verifyToken, userController.profile);
 
-export default router
+export default router;
--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,33 +1,7 @@
-import { Request, Response, NextFunction } from 'express'
-import 'dotenv/config'
-import { IGetUserAuthInfoRequest } from '../custom/custom'
-const jwt = require('jsonwebtoken')
-
-// export const verifyToken = (
-//   req: IGetUserAuthInfoRequest,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   //   //   req.user = {
-//   //   //     name: process.env.TOKEN_KEY,
-//   //   //   };
-
-//   //   next();
-//   //   const token = req.query.token;
-
-//   let token = req.query.token;
-
-//   if (!token) {
-//     return res.status(403).send("A token is required for authentication");
-//   }
-//   try {
-//     const decoded = jwt.verify(token, process.env.TOKEN_KEY);
-//     req.user = decoded;
-//   } catch (err) {
-//     return res.status(401).send("Invalid Token");
-//   }
-//   next();
-// };
+import { Request, Response, NextFunction } from 'express';
+import 'dotenv/config';
+import { IGetUserAuthInfoRequest } from '../custom/custom';
+const jwt = require('jsonwebtoken');
 
 export const verifyToken = (
     req: Request,
@@ -39,27 +13,36 @@ export const verifyToken = (
             return res.status(400).json({
                 success: false,
                 error: 'authorization required',
-            })
+            });
         }
 
-        let token = req.headers.authorization.split(' ')[1]
+        let token = req.headers.authorization.split(' ')[1];
 
-        let decoded = jwt.verify(token, process.env.TOKEN_KEY)
+        jwt.verify(
+            token,
+            process.env.TOKEN_KEY,
+            function (
+                err: any,
+                decoded: { id: string; email: string; fullName: string }
+            ) {
+                if (err) return res.status(400).json({ success: false });
 
-        req.body.user = {
-            id: decoded.id,
-            email: decoded.email,
-            fullName: decoded.fullName,
-        }
+                req.body.user = {
+                    id: decoded.id,
+                    email: decoded.email,
+                    fullName: decoded.fullName,
+                };
+            }
+        );
     } catch (error) {
         return res.status(400).json({
             success: false,
             error: 'server errror',
-        })
+        });
     }
 
-    next()
-}
+    next();
+};
 function next() {
-    throw new Error('Function not implemented.')
+    throw new Error('Function not implemented.');
 }
